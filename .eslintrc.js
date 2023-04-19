f998199e862b27ea10cd270ca6c24c6be40ffcad
Jay Espiritu <juan.aa.espiritu@gmail.com>
2023-04-19 18:39:22 +1000

    chore: upgrade NPM packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,9 @@
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 2019, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module' // Allows for the use of imports
-  },
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:playwright/playwright-test'
-  ]
+	parserOptions: {
+		ecmaVersion: 2019, // Allows for the parsing of modern ECMAScript features
+		sourceType: 'module' // Allows for the use of imports
+	},
+	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+	plugins: ['@typescript-eslint/eslint-plugin'],
+	extends: ['plugin:@typescript-eslint/recommended', 'plugin:playwright/playwright-test']
 };
